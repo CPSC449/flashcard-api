298551e4ec694e9005984c62da0550ef86ae4d4a
Max Ahlgren <maxolof1@hotmail.com>
2026-05-05 16:36:56 -0700

    feat: added connection between user and flashcards

--- src/main/java/com/flashcard/flashcard_api/repository/FlashcardRepository.java
+++ src/main/java/com/flashcard/flashcard_api/repository/FlashcardRepository.java
@@ -3,5 +3,8 @@ package com.flashcard.flashcard_api.repository;
 import com.flashcard.flashcard_api.entity.Flashcard;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface FlashcardRepository extends MongoRepository<Flashcard, String> {
+    List<Flashcard> findByUserId(String userId);
 }
